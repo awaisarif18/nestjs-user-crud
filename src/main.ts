715281fb9f761f6c1971e371a22c71ad60b1d324
Muhammad Awais Arif <sp22-bcs-113@cuilahore.edu.pk>
2023-12-18 18:32:29 +0500

    preflight Continue set true

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
 
       methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
       credentials: true,
+      preflightContinue: true,
       optionsSuccessStatus: 204,
     };
 
