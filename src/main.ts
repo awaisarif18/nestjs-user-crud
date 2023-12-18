0116d40b99d1966375ff144896d9d551ef55290a
Muhammad Awais Arif <sp22-bcs-113@cuilahore.edu.pk>
2023-12-18 14:31:20 +0500

    origin set true

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     const corsOptions: CorsOptions = {
-      origin: '*',
+      origin: true,
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
       optionsSuccessStatus: 204,
