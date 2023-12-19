454484a870a04b8c9967eb2079635cfb55d39a49
Muhammad Awais Arif <sp22-bcs-113@cuilahore.edu.pk>
2023-12-19 12:43:53 +0500

    more methods added

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,10 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use((req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,PUT,POST,DELETE, PATCH, HEAD, OPTIONS',
+      );
       res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
       next();
     });
